7587f3e0c528f4c34ebd03337dbf9b460b84d229
Sonaal1 <sonalgrover8@gmail.com>
2021-03-16 21:36:51 +0530

    commit activity method

--- src/test/java/com/qa/testcases/ActivityClass.java
+++ src/test/java/com/qa/testcases/ActivityClass.java
@@ -4,6 +4,12 @@ public class ActivityClass {
 	public void activity() {
 		System.out.println("Actvity Class");
 	}
+	
+	public void activity2() {{
+		System.out.println("Activity 2");
+	}
+		
+	}
 
 }
 
